bb50ed80c6387b789ad0b0504bb82f668accf5da
Thomas Robertson <tjrobertson@student.unimelb.edu.au>
2022-03-30 12:34:13 +1100

    Priority Queue replaced by heapq

--- search/main.py
+++ search/main.py
@@ -6,14 +6,17 @@ Project Part A: Searching
 # python -m search input.json
 
 # Libraries
-from asyncio import queues
-from collections import defaultdict
+import heapq
 import json
-from queue import PriorityQueue
-
-from numpy import Infinity
-from search.util import print_board, print_coordinate #print_coordinate
 import sys
+
+from collections import defaultdict
+from queue import PriorityQueue
+from numpy import Infinity
+
+from search.util import print_board, print_coordinate #print_coordinate
+
+MOVES = [(1, -1), (1, 0), (0, 1), (-1, 1), (-1, 0), (0, -1)]
 
 # Determine shortest path from start to end nodes on game board
 def main():
@@ -32,71 +35,93 @@ def main():
     goal = tuple(data["goal"])
 
     # Initialise board
-    startState = set()
+    blockedPositions = set()
     for entry in inputBoard:
-        startState.add((entry[1], entry[2]))
+        blockedPositions.add((entry[1], entry[2]))
     
     # Perform A* algorithm, returning the list of nodes from start to goal
-    nodes = aStar(startState, start, goal, n)
+    nodes = aStar(blockedPositions, start, goal, n)
+    if nodes == None:
+        return
     print(len(nodes))
     for node in nodes:
         print(node)
 
+'''
+def ASTAR(start, goal):
+    frontier = PriorityQueue()
+    froniter.put(start, 0)
+    explored = set()
+    while True:
+        if frontier.isempty():
+            return None
+    node = frontier.pop()
+    if node == goal:
+        # return solution
+    explored.add(node)
+    for action in validNeighbours(node)
+
+
+
+
+
+'''
+
+
+
+
 # A* function
-def aStar(blockedPositions, start, goal, n):
+def aStar(explored, start, goal, n):
     # keep track of all nodes parents
     parents = defaultdict(tuple)
-
-    # default all nodes distances to infinity
+    
     def defaultValue():
-        return sys.maxint
+        return sys.maxsize
 
     # g score
     gCosts = defaultdict(defaultValue)
     gCosts[start] = 0
 
     # f score
-    fCosts = defaultdict(defaultValue)
-    fCosts[start] = 0
-    for block in blockedPositions:
+    fCosts = dict()
+    fCosts[start] = h(start, goal)
+    for block in explored:
         fCosts[block] = "-----"
-
-    # heap has O(1) pop performance
-    queue = PriorityQueue()
-    # set has O(1) search performance
-    queueSet = set()
     
-    queue.put(start, 0)
-    queueSet.add(start)
+    # heap has O(log(n)) pop performance
+    frontier = []
+    # set has O(1) search performance
+    frontierSet = set()
+    
+    heapq.heappush(frontier, (0, start))
+    frontierSet.add(start)
 
-    # expand on lowest costing
-    while not queue.empty():
-        # Get the top node in the queue
+    # Expand on lowest costing
+    while len(frontier) > 0:
+        # Get the node with the min f cost from the heap
+        child = heapq.heappop(frontier)[1]
+        frontierSet.remove(child)
         print_board(n, fCosts)
-        current = queue.get()
-        queueSet.remove(current)
 
         # Return the path from the origin to the target
-        if current == goal:
+        if child == goal:
             return reconstruct(parents, goal)
 
         # Go through all neighbours, updating node distances and pushing to the queue & set
-        for neighbour in validNeighbours(blockedPositions, queueSet, current, n):
+        for neighbour in validNeighbours(explored, frontierSet, child, n):
             # + 1 for distance from current node to new node
-            newCostFromOrigin = gCosts[current] + 1
-            if neighbour not in fCosts or newCostFromOrigin < gCosts[neighbour]:
-                parents[neighbour] = current
-                gCosts[neighbour] = newCostFromOrigin
-                fCosts[neighbour] = newCostFromOrigin + h(current, goal)
+            neighbourGCost = gCosts[child] + 1
+            if neighbour not in explored and neighbourGCost < gCosts[neighbour]:
+                parents[neighbour] = child
+                gCosts[neighbour] = neighbourGCost
+                fCosts[neighbour] = neighbourGCost + h(neighbour, goal)
                 
-                # This check is O(n) which is poopy
                 # Could add a set of elems in frontier as well to avoid this check time complexity
-                if neighbour not in queueSet:
-                    queue.put(neighbour, fCosts[neighbour])
-                    queueSet.add(neighbour)
+                if neighbour not in frontierSet:
+                    heapq.heappush(frontier, (fCosts[neighbour], neighbour))
+                    frontierSet.add(neighbour)
     
     return None
-
 
 def reconstruct(parents, goal):
     path = [goal]
@@ -104,79 +129,22 @@ def reconstruct(parents, goal):
         path.append(parents[path[-1]])
     return path[::-1]
 
-def validNeighbours(blockedPositions, queueSet, current, n):
-    moves = [(-1, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, 0)]
+def validNeighbours(explored, frontierSet, current, n):
     out = []
-    for move in moves:
-        cur = tuple(map(sum, zip(current, move)))
-        if cur not in queueSet and cur not in blockedPositions and inBorders(cur, n):
-            out.append(cur)
+    for move in MOVES:
+        newPos = (current[0] + move[0], current[1] + move[1])
+        if inBorders(newPos, n):
+            out.append(newPos)
     return out
 
 def inBorders(pos, n):
     return 0 <= pos[0] < n and 0 <= pos[1] < n
 
+# Returns the manhatten distance between cur and goal
 def h(cur, goal):
-    return abs(cur[0]-goal[0]) + abs(cur[1] - goal[1])
-
-
-
-# class State:
-#     def __init__(self, visited, costs):
-#         self.visited = visited
-#         self.costs = costs
-    
-#     def canMove(self, pos):
-#         return self.visited.contains(pos)
-
-#     def getNeighbours(graph, current):
-
-            
-#     def i
-
-
-# def move(board, parent, pos):
-#     for relativeMove in possibleMoves:
-#         newPos = (pos[0] + relativeMove[0], pos[1] + relativeMove[1])
-#         if isValidMove(board[(newPos[0], newPos[1])])
-        
-        
-#         not board[(newPos[0], newPos[1])]:
-#             move(parent, )
-#     return
-
-
-
-
-# def isValidMove(board, parent, pos)):
-    
-
-
-
-
-'''
-
-- create functions for each action
-- create list of actions
-- create function to validate move
-- create graph traversal algorithm
-- create list for storing nodes
-- print final board
-- make heuristic function
-- make cost path function
-- make goal tester
-- for each node store state, actions, cost to there and heuristic
-- goal counter
-
-
-
-
-- run a star from start node
-- check all possible moves
-- make all possible moves
-
-make move:
-- keep track of parent
-- 
-
-'''
+    dx = goal[0]-cur[0]
+    dy = goal[1]-cur[1]
+    if (dx < 0 and dy < 0) or (dx >= 0 and dy >= 0):
+        return abs(dx + dy)
+    else:
+        return max(abs(dx), abs(dy))
